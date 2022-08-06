4689e6399098a579dccccdf214da8ed36fcd8bf5
gritsly <github.97laj@aleeas.com>
2022-08-06 15:16:42 +0200

    + monit node + user playbook + swapon

--- terraform/stakeWars-node01.tf
+++ terraform/stakeWars-node01.tf
@@ -23,9 +23,24 @@ resource "hcloud_ssh_key" "mysshkey" {
 resource "hcloud_server" "stakeWars-node01" {
   name        = "stakeWars-node01"
   image       = "ubuntu-22.04"
-  datacenter  = "fsn1-dc14"
+  datacenter  = "fsn1"
   ssh_keys    = ["mysshkey"]
-  server_type = "cpx31"
+  server_type = "cpx41"
+  keep_disk   = true
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+  depends_on = [hcloud_ssh_key.mysshkey]
+}
+
+resource "hcloud_server" "stakeWars-monit" {
+  name        = "stakeWars-monit"
+  image       = "ubuntu-22.04"
+  datacenter  = "hel1"
+  ssh_keys    = ["mysshkey"]
+  server_type = "cpx11"
+  keep_disk   = true
   public_net {
     ipv4_enabled = true
     ipv6_enabled = true
